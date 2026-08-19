52e77671feb989db7cdc753aa8da261fb62e658d
sriniballij-lab <srini.ballij@gmail.com>
2026-08-18 21:08:40 -0400

    made a changes

--- test_math.py
+++ test_math.py
@@ -1,5 +1,14 @@
 def test_addition():
     result = 2+3
     assert result ==5
-    
-    
+def test_subtraction():
+    result = 5-3
+    assert result ==2
+
+def test_multiplication():
+    result = 2*3
+    assert result ==6
+
+def test_division():
+    result = 6/2
+    assert result ==3
