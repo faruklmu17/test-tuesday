559b73d5ae40fc6de0dce615cc8d584ce781c2dc
sriniballij-lab <srini.ballij@gmail.com>
2026-08-25 20:50:12 -0400

    made divide function

--- calculator.py
+++ calculator.py
@@ -7,5 +7,5 @@ def multiply(a,b):
 def divide(a,b):
   if b==0:
     raise ValueError("We cannot divide by zero")
-    
-  return a/b
+
+    return a/b
--- test_calculator.py
+++ test_calculator.py
@@ -3,3 +3,10 @@ from calculator import add, subtract, multiply, divide
 
 def test_add():
   assert add(2,4)==6
+
+
+
+
+
+def test_divide():
+  assert divide(4,2)==2
